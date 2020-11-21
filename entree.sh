044a5fdc52dfc5621c13f6b822bdd69f21c7b7b3
Meandres <ilya.meignan-masson@mailoo.org>
2020-11-21 17:38:33 +0100

    resolutions de bugs sur entree.sh

--- entree.sh
+++ entree.sh
@@ -9,29 +9,31 @@ for champ in $*
 do
 	champsNecessaires[$champ]=0
 done
+
 echo "Vous pouvez quitter à tout moment en entrant quit ou exit."
 continuer="O"
 while [ $continuer = "O" ]
 do
-	echo "blbl"
 	echo "Veuillez entrer le nom du champ."
 	read champ		
 	echo "Veuillez entrer le contenu du champ."
 	read contenu
-
+	echo $champsPossibles " " $champ
+	grep $champ <<< $champsPossibles
 	[ $champ="quit" -o $champ="exit" ] && exit 0
-	if [ $champsPossibles = *$champ* ] then
-		ecrire=$ecrire + "$champ = $contenu"
-		#[ $champ in $* ] && champsNecessaires[$champ]=1
+	if grep -q "$champ" <<< $champsPossibles; then
+		echo "champ reconnu"
+		ecrire+="\t $champ \t $contenu \n";
+		[ $champ in ${!champsNecessaires[*]} ] && champsNecessaires[$champ] = 1 
 	else
-		echo "Le champ que vous avez entré n'est pas valide."
+		"Le champ que vous avez entré n'est pas valable."
 	fi
 	correct="true"
-	for champ in $*
+	for requis in ${champsNecessaires[*]}
 	do
-		[ champsNecessaires[$champ] -eq 0 ] && correct="false"
+		[ requis -eq 0 ] && correct="false"
 	done
-	if [ $correct = "true" ] then
+	if [ $correct = "true" ]; then
 		echo "Tout les champs nécessaires ont été remplis. Voulez-vous continuer (O/n) ?"
 		read continuer
 	fi
