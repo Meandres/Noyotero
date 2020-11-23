e05df6c9e01aafcde0a975b8f8c7b4799edfc990
Meandres <ilya.meignan-masson@mailoo.org>
2020-11-23 18:21:45 +0100

    entree.sh finie (sauf bug insoupçonnés)

--- entree.sh
+++ entree.sh
@@ -1,39 +1,64 @@
 #!/bin/bash
 
+champEntre(){
+	str=`cat tmp | grep $1`
+	c=${str:0:${#str}-1}
+	if [ -n "$str" ]; then
+		echo "Champ nécessaire reconnu"
+		set `cat tmp | grep -v $1`
+		cat /dev/null > tmp
+		for ligne in $*
+		do
+			echo $ligne >> tmp
+		done
+		echo "$c""1" >> tmp
+	fi
+}
+verifierChamp(){
+	valide=0
+	while read ligne
+	do
+		[[ $ligne == *"0" ]] && { valide=1; break; }
+	done < tmp
+}
+
 choix=$1
 ecrire="@$choix{\n"
-set `cat champsNecessaires.txt | grep $choix`
-shift
+champsNecessaires=`cat champsNecessaires.txt | grep ^$choix`
 champsPossibles=`cat champsPossibles.txt`
-for champ in $*
+cat /dev/null > tmp
+for champ in $champsNecessaires
 do
-	champsNecessaires[$champ]=0
+	[ $champ != $choix ] && echo -e "$champ""0" >> tmp
 done
-echo ${!champsNecessaires[@]}
+
 echo "Vous pouvez quitter à tout moment en entrant quit ou exit."
 continuer="O"
 while [ $continuer = "O" ]
 do
 	echo "Veuillez entrer le nom du champ."
-	read champ		
+	read champ
 	[[ $champ = "quit" || $champ = "exit" ]] && exit 0
-	echo "Veuillez entrer le contenu du champ."
-	read contenu
-	if [[ $champsPossibles == *"$champ"* ]]; then
-		echo "champ reconnu"
-		ecrire+="\t$champ\t$contenu\n"
-		[ $champ in ${!champsNecessaires[*]} ] && champsNecessaires[$champ]=1
+	if [[ $champsEntres == *"$champ"* ]]; then
+		echo "Vous avez déjà renseigné ce champ."
 	else
-		echo "Le champ que vous avez entré n'est pas valable."
+		champsEntres+=$champ
+		echo "Veuillez entrer le contenu du champ."
+		read contenu
+		if [[ $champsPossibles == *"$champ"* ]]; then
+			echo "Champ reconnu"
+			ecrire+="\t$champ\t$contenu\n"
+			champEntre $champ
+		else
+			echo "Le champ que vous avez entré n'est pas valable."
+		fi
 	fi
-	correct="true"
-	for requis in ${champsNecessaires[*]}
-	do
-		[ $requis -eq 0 ] && correct="false"
-	done
-	if [ $correct = "true" ]; then
+	verifierChamp
+	echo ""
+	if [ $valide -eq 0 ]; then
 		echo "Tout les champs nécessaires ont été remplis. Voulez-vous continuer (O/n) ?"
 		read continuer
 	fi
 done
+rm tmp
 echo -e $ecrire
