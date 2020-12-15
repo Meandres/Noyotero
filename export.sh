7350f025e6927ac2684c8228d9cfd4e67b1085ef
Meandres <ilya.meignan-masson@mailoo.org>
2020-12-15 11:47:54 +0100

    version de l'exportation aboutie. Avant correctifs

--- export.sh
+++ export.sh
@@ -2,28 +2,45 @@
 
 exportPdf(){
 	cp modeleLaTeX.tex references.tex
-	PligneLue="n"
 	while read ligne
 	do
-		preLet=${ligne:0:1}
-		[[ $preLet == % ]] & { echo "comm"; continue; }
-		if [[ $preLet == @ ]]; then
-			echo "blbl"
-			if [ $PligneLue = "n" ]; then
-				PligneLue="o"
-			else	
-				echo '\\hline' > references.tex
-				echo '\\end{tabular}' > references.tex
-			fi
-			echo '\\begin{tabular}{| c | c |}' > references.tex
-			echo '\\hline' > references.tex
+		if [[ $ligne == @* ]]; then
+			echo '\section{}' >> references.tex
 		else
-			IFS="="
-			set $ligne
-			echo  $1 " & " $2 ' \\\\ \\hline' > references.tex
-			IFS=" "
+			if [[ $ligne == '}' ]]; then continue
+			fi
+			contenu=`echo "$ligne" | sed 's/_/$\\\_$/g'`
+			echo  $contenu '\\' >> references.tex
 		fi
 	done < bd.bib
+	echo '\end{document}' >> references.tex
+
+	pdflatex references.tex > /dev/null
+	mv references.pdf references$1.pdf
+	rm references.*
+}
+
+exportHtml(){
+	cp modeleHtml.html references$1.html
+	PLigneLue="n"
+	while read ligne
+	do
+		if [[ $ligne == @* ]]; then
+			if [[ $PLigneLue == "n" ]]; then
+				PLigneLue="o"
+			else
+				echo '</p>' >> references$1.html
+			fi
+			echo '<p>' >> references$1.html
+		else
+			if [[ $ligne == '}' ]]; then continue
+			fi
+			echo  $ligne '<br>' >> references$1.html
+		fi
+	done < bd.bib
+	echo '</body>' >> references$1.html
+	echo '</html>' >> references$1.html
+
 }
 
 continuer="O"
@@ -32,7 +49,7 @@ while [ $continuer = "O" ]
 do
 	echo "Vers quel format souhaitez-vous exporter (1-2-3) ? (plusieurs choix possibles ex : 1 2)."
 	echo "1. Vers un .txt"
-	echo "2. Vers un .pdf (en passant par un fichier LaTeX)"
+	echo "2. Vers un .pdf (en passant par un fichier LaTeX, nécessite pdflatex)"
 	echo "3. Vers un .html"
 	read choix
 	dateExport=`date +"%Y%m%d_%Hh%Mm%S"`
@@ -40,9 +57,9 @@ do
 		cp bd.bib references$dateExport.txt
 	fi
 	if [[ $choix == *2* ]]; then
-		exportPdf
+		exportPdf $dateExport
 	fi
 	if [[ $choix == *3* ]]; then
-		exportHtml	
+		exportHtml $dateExport
 	fi
 done
