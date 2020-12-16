289245d2f36d48f90079ae4e8262a9d7f5399746
Meandres <ilya.meignan-masson@mailoo.org>
2020-12-16 09:09:39 +0100

    modif d'import

--- import.sh
+++ import.sh
@@ -3,18 +3,20 @@
 echo "Importation depuis un fichier .xml (nécessite libxml-xpath-perl)"
 echo "Entrez le nom du fichier depuis lequel vous voulez importer les references : (sans l'extension)"
 read nomfic
-[ ! $nomfic -f ] & { echo "Le nom que vous avez entré n'est pas valide."; exit 0; }
 LecRef="n"
 while read ligne
 do
-  [[ $ligne == "<reference>" ]] & { LecRef="o"; echo '@Article {' >> bd.bib; }
-  [[ $ligne == "</reference>" ]] & { LecRef="n"; echo '}' >> bd.bib; }
-  if [[ $LecRef == "o" ]]; then
-    contenu=`echo $ligne | tr "<>" " "`
-    IFS="/"
-    set $contenu
-    contenu=$1
-    echo $contenu | sed "s/ //" | sed "s/ /=/" >> bd.bib
-  fi
+	echo "_"$ligne"_"
+	[[ $ligne == *"<reference>"* ]] & { LecRef="o"; echo '@Article {' >> testimport.bib; }
+	[[ $ligne == *"</reference>"* ]] & { LecRef="n"; echo '}' >> testimport.bib; }
+	echo $LecRef
+	if [[ $LecRef == "o" ]]; then
+		echo "ligne"
+		contenu=`echo $ligne | tr "<>" " "`
+		IFS="/"
+		set $contenu
+		contenu=$1
+		echo $contenu | sed "s/ //" | sed "s/ /=/" >> testimport.bib
+	fi
 done < $nomfic
 IFS=" "
